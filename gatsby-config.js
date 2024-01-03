748661fc62ba127205b05ed31b1fed04c47b0068
Ian Wang <ianuw@uw.edu>
2024-01-02 22:32:53 -0800

    update/clean files

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  // pathPrefix:`/UW-AACF-Website`,
+  pathPrefix:`/UW-HTTC`,
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-transformer-remark',
--- src/pages/members/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Members() {
-  return (
-    <div>
-        <h1>Members</h1>
-    </div>
-  )
-}
